55c55a524dad31a6f163750fbdca1ecde162c625
Rubén M <dev.mantecon@gmail.com>
2021-06-18 23:20:32 +0200

    collections

--- exercises/collections/vec2.rs
+++ exercises/collections/vec2.rs
@@ -4,15 +4,13 @@
 //
 // Make me pass the test!
 //
-// Execute the command `rustlings hint vec2` if you need
+// Execute the command `rustlings hint collections2` if you need
 // hints.
 
-// I AM NOT DONE
 
 fn vec_loop(mut v: Vec<i32>) -> Vec<i32> {
     for i in v.iter_mut() {
-        // TODO: Fill this up so that each element in the Vec `v` is
-        // multiplied by 2.
+        *i *= 2
     }
 
     // At this point, `v` should be equal to [4, 8, 12, 16, 20].
@@ -28,6 +26,11 @@ mod tests {
         let v: Vec<i32> = (1..).filter(|x| x % 2 == 0).take(5).collect();
         let ans = vec_loop(v.clone());
 
-        assert_eq!(ans, v.iter().map(|x| x * 2).collect::<Vec<i32>>());
+        assert_eq!(
+            ans,
+            v.iter()
+                .map(|x| x * 2)
+                .collect::<Vec<i32>>()
+        );
     }
 }
