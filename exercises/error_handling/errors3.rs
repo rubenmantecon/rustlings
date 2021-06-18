2b66d40ca8d93a0ff531f7d9a3bc9f9d40f5808d
Rubén M <dev.mantecon@gmail.com>
2021-06-18 23:23:57 +0200

    error handling

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -4,15 +4,17 @@
 // Why not? What should we do to fix it?
 // Execute `rustlings hint errors3` for hints!
 
-// I AM NOT DONE
+
+//unwrap() has been used to successfully compile the program. But, on the docs, it seems that declaring main to have a Result return type was the way to go (even the hint seemed to suggest so)
 
 use std::num::ParseIntError;
+use std::error::Error;
 
 fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
+    let cost = total_cost(pretend_user_input).unwrap();
 
     if cost > tokens {
         println!("You can't afford that many!");
